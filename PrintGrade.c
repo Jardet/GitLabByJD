b6956c5ccfecfd41da22cce82de669577f79e99b
Jardet <jarude555@gmail.com>
2018-11-20 15:22:41 +0700

    I change some code in PrintGrade.c

--- PrintGrade.c
+++ PrintGrade.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 void main()
 {
-  char Name[30][40] = {"Jarudet Wichit", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"}; 
+  char Name[30][40] = {"Jarudet Wichit", "Mary McBeth", "Peter Jackson","Chai Chanchai", "Yong Boonsith", "Prayuth Chanosha"}; 
   int ID[30] = {5301, 5302, 5303, 5304, 5305, 5306}; 
   float GPA[30] = {2.73, 2.35, 3.65, 3.24, 1.32, 3.26}; 
   int N = 6, i;
-  int SearchID, found_index; 
+  double SearchID, found_index; 
  
-  printf("\nSTUDENT GPA LIST: \n"); 
-  printf("ID----Name------------------------GPA\n"); 
+  for(i-0;i<20;i++)
+    printf("I'm sleepy");
   for (i=0;i<N; i++)
     printf("%5d %-25s %5.2f\n", ID[i], Name[i], GPA[i]); 
   printf("-------------------------------------\n");
